4f51ac23c995d9e2080060239d2823f506d04e35
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-02-02 02:03:56 -0500

    follow livelist.js master

--- lib/livelist/rails/version.rb
+++ lib/livelist/rails/version.rb
@@ -1,7 +1,7 @@
 module Livelist
   module Rails
     VERSION = '0.0.9'
-    LIVELIST_VERSION = '0.0.4'
+    LIVELIST_VERSION = '6a3f723bda3ccec77d1952c075775d6664547696'
     MUSTACHE_VERSION = 'db5f5ece0b6c87bbb2d0584010b97f8723dde69d'
     UNDERSCORE_VERSION = '1.2.3'
   end
