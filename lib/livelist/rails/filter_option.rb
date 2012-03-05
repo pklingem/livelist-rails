b222040fa9218e51305543751ea43a7dea5f4897
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-03-05 13:29:08 -0500

    work towards setting the filter option type

--- lib/livelist/rails/filter_option.rb
+++ lib/livelist/rails/filter_option.rb
@@ -1,14 +1,23 @@
+require 'active_record'
+
 module Livelist
   module Rails
 
     class FilterOption
-      attr_accessor :slug, :name, :count, :value
+      attr_accessor :slug,
+                    :name,
+                    :count,
+                    :value,
+                    :type,
+                    :name_key
 
       def initialize(options = {})
+        @filter            = options[:filter]
         @option_collection = options[:option_collection]
+        @name_key          = infer_name_key(options[:option], options[:name_key])
+        @type              = infer_type(options[:option])
         @slug              = infer_slug(options[:option])
         @name              = infer_name(options[:option])
-        @filter            = options[:filter]
       end
 
       def selected?(params)
@@ -27,23 +36,40 @@ module Livelist
 
     private
 
-      def infer_slug(option)
-        if [String, Symbol, Integer].any?{|klass| option.kind_of?(klass)}
-          option
+      def infer_name_key(option, name_key)
+        if name_key
+          name_key
+        elsif option.respond_to?(@option_collection.slug)
+          @option_collection.slug
         else
-          option[@option_collection.slug]
+          :name
+        end
+      end
+
+      def infer_type(option)
+        if [String, Symbol, Integer].any?{|klass| option.kind_of?(klass)}
+          :scalar
+        elsif option.kind_of?(Hash) && option.has_key?(@name_key)
+          :hash
+        elsif option.respond_to?(@name_key)
+          :model
+        else
+          raise ArgumentError, "option is not valid \n #{option.inspect}"
+        end
+      end
+
+      def infer_slug(option)
+        case @type
+        when :scalar       then option
+        when :hash, :model then option[@option_collection.slug]
         end
       end
 
       def infer_name(option)
-        if [String, Symbol, Integer].any?{|klass| option.kind_of?(klass)}
-          option
-        elsif option.kind_of?(Hash) && option.has_key?(:name)
-          option[:name]
-        elsif option.respond_to?(:name)
-          option.name
-        else
-         option[@option_collection.slug]
+        case @type
+        when :scalar then option
+        when :hash   then option[@name_key]
+        when :model  then option.send(@name_key)
         end
       end
     end
