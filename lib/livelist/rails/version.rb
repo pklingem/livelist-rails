d98952bf8b6b345eda2af6935e69f7a9ef9b8aee
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-03-20 13:23:13 -0400

    bump version

--- lib/livelist/rails/version.rb
+++ lib/livelist/rails/version.rb
@@ -1,6 +1,6 @@
 module Livelist
   module Rails
-    VERSION = '0.0.10'
+    VERSION = '0.0.11'
     LIVELIST_VERSION = '73a5124e4aee536963e90935f02566d67030d2ea'
     MUSTACHE_VERSION = 'db5f5ece0b6c87bbb2d0584010b97f8723dde69d'
     UNDERSCORE_VERSION = '1.2.3'
