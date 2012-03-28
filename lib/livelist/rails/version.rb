14d2309adb56739ad61c95251a98cec3a149911b
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-03-28 00:10:01 -0400

    update underscore.js

--- lib/livelist/rails/version.rb
+++ lib/livelist/rails/version.rb
@@ -3,6 +3,6 @@ module Livelist
     VERSION = '0.0.12'
     LIVELIST_VERSION = '0.0.6'
     MUSTACHE_VERSION = '0.4.2'
-    UNDERSCORE_VERSION = '1.2.3'
+    UNDERSCORE_VERSION = '1.3.1'
   end
 end
