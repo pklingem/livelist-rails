ca3e0f2ecb4e9b7a2b1235858e7a2cf0ec4a71ad
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-03-28 00:31:40 -0400

    bump livelist.js to 0.0.7

--- lib/livelist/rails/version.rb
+++ lib/livelist/rails/version.rb
@@ -1,7 +1,7 @@
 module Livelist
   module Rails
     VERSION = '0.0.12'
-    LIVELIST_VERSION = '0.0.6'
+    LIVELIST_VERSION = '0.0.7'
     MUSTACHE_VERSION = '0.4.2'
     UNDERSCORE_VERSION = '1.3.1'
   end
